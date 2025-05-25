a6099cd37fc768c7d049956e88d5472fb59cf9fb
DucDat <luuducdat05042004@gmail.com>
2025-05-25 22:27:31 +0700

    hien list bug

---
+++ src/pages/Technical/BugList.js
@@ -0,0 +1,14 @@
+import '../../styles/TechnicalHome.css';
+import TechnicalHome from '../../components/TechnicalHome';
+
+function Home(){
+    return(
+        <div>
+            {/* <Header></Header> */}
+            <div className="content">
+            <TechnicalHome></TechnicalHome>
+            </div>
+        </div>
+    );
+};
+export default Home;
---
+++ src/pages/Technical/BugResponse.js
@@ -0,0 +1,14 @@
+import '../../styles/TechnicalHome.css';
+import TechnicalHome from '../../components/TechnicalHome';
+
+function ReplyBug(){
+    return(
+        <div>
+            {/* <Header></Header> */}
+            <div className="content">
+            <TechnicalHome></TechnicalHome>
+            </div>
+        </div>
+    );
+};
+export default ReplyBug;
